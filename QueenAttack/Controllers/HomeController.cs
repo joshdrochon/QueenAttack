9294bfa5a0a871f5d1eaec1d3fb54c7be8763c78
Josh Rochon <joshdrochon@gmail.com>
2018-02-13 16:40:45 -0800

    functional

---
+++ QueenAttack/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using QueenAttackProject.Models;
+using System;
+
+namespace QueenAttackProject.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("QueenAttack/QueenAttackResult")]
+    public ActionResult Result()
+    {
+      int inputX1 = Int32.Parse(Request.Form["queen-x"]);
+      int inputY1 = Int32.Parse(Request.Form["queen-y"]);
+
+      int inputX2 = Int32.Parse(Request.Form["target-x"]);
+      int inputY2 = Int32.Parse(Request.Form["target-y"]);
+
+      QueenAttack newQueenAttack = new QueenAttack();
+
+      bool myQueenAttack = newQueenAttack.CanAttack
+      (inputX1, inputY1, inputX2, inputY2);
+
+      if (myQueenAttack == false)
+      {
+        return View("../QueenAttack/ResultFalse");
+      }
+      else
+      {
+        return View("../QueenAttack/ResultTrue");
+      }
+    }
+
+  }
+}
